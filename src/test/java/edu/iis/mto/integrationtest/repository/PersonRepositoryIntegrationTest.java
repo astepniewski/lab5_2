721cf28cbeea1b2039725e353ad7c15ead99b1ea
Arkadiusz Stępniewski <a.stepniewski@hotmail.com>
2015-04-27 12:32:29 +0200

    Dodanie adnotacji @Autowired do personRepository w teście oraz dodanie adnotacji do Integration test

--- src/test/java/edu/iis/mto/integrationtest/repository/PersonRepositoryIntegrationTest.java
+++ src/test/java/edu/iis/mto/integrationtest/repository/PersonRepositoryIntegrationTest.java
@@ -6,13 +6,14 @@ import static org.junit.Assert.assertEquals;
 import java.util.List;
 
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import edu.iis.mto.integrationtest.model.Person;
 
 
 public class PersonRepositoryIntegrationTest extends IntegrationTest {
 
-	
+	@Autowired
 	private PersonRepository personRepository;
 
 	@Test
